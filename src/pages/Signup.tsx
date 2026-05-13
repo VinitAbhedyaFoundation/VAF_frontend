d0c627f7ccbec34b523b0ac2f967ac0e37dc4b37
priyaghadge34-lang <priyaghadge34@gmail.com>
2026-05-13 23:04:16 +0530

    Fixed minor API issues

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -85,7 +85,7 @@ const Signup = () => {
     setError("");
 
     try {
-      const res = await fetch("http://localhost:3000/auth/register", {
+      const res = await fetch("http://localhost:3000/api/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
